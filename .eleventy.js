2a4fd8901fccf03304bd21549a0e88715d509c04
MK <mihir2726@gmail.com>
2021-06-26 19:17:08 +0530

    fixed cms

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/assets");
 
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
   eleventyConfig.addFilter("postDate", (dateObj) => {
-
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-
   });
 
   return {
